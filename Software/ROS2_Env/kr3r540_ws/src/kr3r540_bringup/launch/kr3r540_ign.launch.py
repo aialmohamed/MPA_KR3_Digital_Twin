5fa6a267b361ccb7d393d34d7e69ff8af8db9976
aialmohamed <ahmaedibrahim311@gmail.com>
2024-10-11 20:41:35 +0200

    update : ros2 control is running

--- Software/ROS2_Env/kr3r540_ws/src/kr3r540_bringup/launch/kr3r540_ign.launch.py
+++ Software/ROS2_Env/kr3r540_ws/src/kr3r540_bringup/launch/kr3r540_ign.launch.py
@@ -1,34 +1,37 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription,SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription,SetEnvironmentVariable,ExecuteProcess,RegisterEventHandler
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import PathJoinSubstitution, Command, LaunchConfiguration, FindExecutable
+from launch.event_handlers import OnProcessExit
+from pprint import pprint
+
+import xacro
 
 def generate_launch_description():
-    # Declare launch arguments for paths
     urdf_path = LaunchConfiguration("urdf_path")
     world_path = LaunchConfiguration("world_path")
     rviz_config_path = LaunchConfiguration("rviz_config_path")
 
-    # Get the package directories for required files
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
     pkg_kr3r540_description = get_package_share_directory("kr3r540_description")
     pkg_kr3r540_bringup = get_package_share_directory("kr3r540_bringup")
 
-    # Define the paths to the URDF, RViz, and world files using the package directories
+    
+
     declare_urdf_path = DeclareLaunchArgument(
         "urdf_path",
         default_value=PathJoinSubstitution(
-            [pkg_kr3r540_description, "urdf", "kr3r540.urdf.xacro"]
+            [pkg_kr3r540_description, "urdf", "kr3r540.urdf"]
         ),
         description="Path to the URDF file of the robot."
     )
 
     declare_world_path = DeclareLaunchArgument(
         "world_path",
-        default_value=PathJoinSubstitution([pkg_kr3r540_description, "world", "default_world.sdf"]),
+        default_value=PathJoinSubstitution([pkg_kr3r540_description, "world", "kr3r540_world.sdf"]),
         description="Path to the Ignition world file."
     )
 
@@ -38,55 +41,49 @@ def generate_launch_description():
         description="Path to the RViz config file."
     )
 
-    # Generate robot_description parameter by calling xacro
-    robot_description_content = Command([
-        FindExecutable(name='xacro'),
-        ' ',
-        urdf_path,
-    ])
 
-    robot_description = {'robot_description': robot_description_content}
 
     SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', PathJoinSubstitution([get_package_share_directory('kr3r540_description'),"meshes"]))
 
-
-    # Node to publish the robot's URDF
+    
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[robot_description],
+        parameters=[{"use_sim_time": True}],
+        arguments=[urdf_path],
+        output='screen',
     )
 
-    # Joint State Publisher node for publishing joint states
+
     joint_state_publisher_node = Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
         output="screen",
     )
 
-    # RViz Node to visualize the robot model
+
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
-        arguments=['-d', rviz_config_path],
+        parameters=[{"use_sim_time": True}],
+        arguments=['-d', rviz_config_path ,'use_sim_time','true'],
         output="screen",
     )
 
-    # Include launch file to start Ignition Gazebo with the specified world
+
     ignition_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])]),
         launch_arguments={
-            'gz_args': PathJoinSubstitution([
-                pkg_kr3r540_description, 'world', 'default_world.sdf'
-            ]),
+            'gz_args': world_path,
             'on_exit_shutdown': 'true'
             }.items(),
     )
 
-    # Node to spawn the robot in Gazebo
+
     spawn_entity = Node(
     package='ros_gz_sim',
     executable='create',
+    parameters=[{"use_sim_time": True}],
     arguments=[
         '-file', urdf_path,
         '-name', 'kr3r540',
@@ -94,9 +91,33 @@ def generate_launch_description():
     ],
     output='screen'
     )
-    
 
-    # Return the launch description with all the nodes and configurations
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+    
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_trajectory_controller'],
+        output='screen'
+    )
+    spawn_entity_reg = RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        )
+
+    
+    load_joint_state_broadcaster_reg = RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        )
+
     return LaunchDescription([
         declare_urdf_path,
         declare_world_path,
@@ -105,5 +126,7 @@ def generate_launch_description():
         joint_state_publisher_node,
         rviz_node,
         ignition_launch,
+        spawn_entity_reg,
+        load_joint_state_broadcaster_reg,
         spawn_entity,
     ])
